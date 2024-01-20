a8c4c6c84c363832013f4143f7d9e3cc4f11ec7b
AlexandreBottet <alexandre.bottet@outlook.fr>
2024-01-20 16:05:36 +0100

    refactor: Add Pokemon component child to Pokemons

--- src/components/Pokemons/index.tsx
+++ src/components/Pokemons/index.tsx
@@ -1,21 +1,12 @@
+import Pokemon from './Pokemon';
+
 import './Pokemons.css';
 
 const Pokemons = () => {
 
     return (
         <div className="pokemons">
-            <div className="pokemon">
-                <img className="pokemon_picture" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur" />
-                <h2 className="pokemon_name">#1 Bulbasaur</h2>
-            </div>
-            <div className="pokemon">
-                <img className="pokemon_picture" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur" />
-                <h2 className="pokemon_name">#1 Bulbasaur</h2>
-            </div>
-            <div className="pokemon">
-                <img className="pokemon_picture" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur" />
-                <h2 className="pokemon_name">#1 Bulbasaur</h2>
-            </div>
+            <Pokemon />
         </div>
     );
 };
