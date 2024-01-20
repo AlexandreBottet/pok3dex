891df04cfeca8214e125d2c848467df27c113e70
AlexandreBottet <alexandre.bottet@outlook.fr>
2024-01-20 15:58:03 +0100

    feat: Add Pokemons component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 
 import Header from './components/Header';
+import Pokemons from './components/Pokemons';
 
 const App = () => {
 
   return (
     <>
       <Header />
+      <Pokemons />
     </>
   );
 };
